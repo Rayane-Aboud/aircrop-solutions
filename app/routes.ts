c78becb9b8e8c2468a86614d4e0919fdbde075d9
Rayane-Aboud <jr_aboud@esi.dz>
2025-05-14 23:02:28 +0100

    initial repo

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    //home route
+    index("./pages/home/home.tsx"),
+
+    //about route
+    route("about","./pages/about/about.tsx"),
+
+    //products route
+    route("products","./pages/products/products.tsx"),
+
+
+] satisfies RouteConfig;
